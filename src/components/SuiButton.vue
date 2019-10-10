b5a9cf067ef942e0fcb1e71c26156b16a5c9d379
Lukas Figura <>
2019-10-10 11:32:52 +0200

    chore: update eslint & prettier config

--- src/components/SuiButton.vue
+++ src/components/SuiButton.vue
@@ -1,108 +1,32 @@
 <template>
-  <button
-    :class="[
-      'nice-handsome-button',
-      'nice-handsome-button--' + color,
-      'nice-handsome-button--' + size,
-      {
-        'nice-handsome-button--rounded': rounded
-      }
-    ]"
-    @click="onClick"
-    @dblclick="onDoubleClick"
-  >
-    <slot />
+  <button @click="onClick" @dblclick="onDoubleClick">
+    <slot>
+      Button
+    </slot>
   </button>
 </template>
 
 <script>
 export default {
-  name: "SuiButton",
+  name: 'SuiButton',
+
   props: {
-    color: {
-      type: String,
-      default: "blue",
-      validator(x) {
-        return ["blue", "green", "red"].indexOf(x) !== -1;
-      }
-    },
     rounded: {
       type: Boolean,
-      default: true
-    },
-    size: {
-      type: String,
-      default: "default",
-      validator(x) {
-        return ["small", "default", "large"].indexOf(x) !== -1;
-      }
+      default: false
     }
   },
 
   methods: {
     onClick(event) {
-      this.$emit("click", event);
+      this.$emit('click', event)
     },
 
     onDoubleClick(event) {
-      this.$emit("dblclick", event);
+      this.$emit('dblclick', event)
     }
   }
-};
+}
 </script>
 
-<style scoped>
-.nice-handsome-button {
-  display: inline-block;
-  outline: 0;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  color: #ffffff;
-  font-weight: 500;
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-  user-select: none;
-  cursor: pointer;
-}
-
-/* --> COLORS <-- */
-
-.nice-handsome-button--blue {
-  background-color: #0194ef;
-}
-
-.nice-handsome-button--green {
-  background-color: #1bb934;
-}
-
-.nice-handsome-button--red {
-  background-color: #e1112c;
-}
-
-/* --> SIZES <-- */
-
-.nice-handsome-button--small {
-  padding: 8px 10px;
-  border-radius: 4px;
-  font-size: 12px;
-  line-height: 12px;
-}
-
-.nice-handsome-button--default {
-  padding: 12px 14px;
-  border-radius: 6px;
-  font-size: 14px;
-  line-height: 16px;
-}
-
-.nice-handsome-button--large {
-  padding: 16px 18px;
-  border-radius: 8px;
-  font-size: 16px;
-  line-height: 20px;
-}
-
-/* --> BOOLEANS <-- */
-
-.nice-handsome-button--rounded {
-  border-radius: 60px;
-}
-</style>
+<style lang="postcss" scoped></style>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import SuiButton from "./components/SuiButton";
 
 export default {
-    // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars
   install(Vue, options) {
     // Let's register our component globally
     // https://vuejs.org/v2/guide/components-registration.html
